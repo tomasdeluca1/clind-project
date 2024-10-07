24f57bf21b1505b454591e05d68714cac91d555a
Tomás Deluca <Tomasdelucaa@gmail.com>
2024-10-07 19:50:37 -0300

    changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -26,7 +26,12 @@ export default function Layout({ children }) {
               <div className="dropdown dropdown-end">
                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                   <div className="w-10 rounded-full">
-                    <img src={user.picture} alt={user.name} />
+                    <Image
+                      src={user.picture}
+                      alt={user.name}
+                      width={50}
+                      height={50}
+                    />
                   </div>
                 </label>
                 <ul
