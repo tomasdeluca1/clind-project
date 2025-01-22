77ef81731087d660dcbe139ef578822ede811f14
Tomás Deluca <Tomasdelucaa@gmail.com>
2025-01-22 14:12:37 -0300

    umami tracking

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 function MyDocument() {
   return (
     <Html lang="en" data-theme="emerald">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c6d29b68-5d43-4ebf-96cb-74b8ca555a80"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
